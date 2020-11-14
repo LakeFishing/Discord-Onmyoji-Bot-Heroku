b855ea4861e06fd9d3b1fa4a04df33fde1b54ed9
LakeFishing <yu.ting.melt@gmail.com>
2020-11-14 23:14:06 +0800

    2020/11/14

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from io import BytesIO
 
 from PIL import Image
 
-import rare
+import newrare
 
 with open("setting.json","r",encoding="utf-8") as jFile_1:
     jdata_1 = json.load(jFile_1)
@@ -111,6 +111,7 @@ async def on_message(msg):
 
     await bot.process_commands(msg)
 
+"""
 @bot.command()
 async def 十連抽(msg):
 
@@ -154,12 +155,39 @@ async def 十連抽(msg):
             embed.add_field(name="> R 數量", value="```" + str(rare_list[3]) + "```", inline=True)
             embed.set_footer(text="暫時無法生成抽卡結果圖片\n有任何問題或建議請找 YellowToFish#5671")
             await msg.channel.send(embed = embed)
+"""
 
 @bot.command()
-async def test(msg):
-    aimage = Image.open("./icons/SSR/217.png")
-    aimage.save("0000.png")
-    pic = discord.File("0000.png")
-    await msg.channel.send(file=pic)
+async def rolltest(msg):
+
+    rolledchannellist = list(jdata_1["RolledChannel"])
+
+    if msg.channel.id in rolledchannellist:
+
+        toImage = Image.new('RGBA',(600,240),color="white")
+        pic_list, pic_num, rare_list = newrare.Rolled()
+
+    for i in range(10):
+        try:
+            fromImage = Image.open(pic_list[i])
+            loc = ((int(i/2) * 120), (i % 2) * 120)
+            toImage.paste(fromImage, loc)
+            fromImage.close()
+        except:
+            break
+    
+    save_name = str(pic_num) + ".png"
+    toImage.save(save_name)
+    toImage.close()
+    file = discord.File(save_name, filename="image.png")
+    
+    embed = discord.Embed(title="抽卡結果", color=0xffff00)
+    embed.add_field(name="> SP & SSR 數量", value="```" + str(rare_list[0] + rare_list[1]) + "```", inline=False)
+    embed.add_field(name="> SR 數量", value="```" + str(rare_list[2]) + "```", inline=True)
+    embed.add_field(name="> R 數量", value="```" + str(rare_list[3]) + "```", inline=True)
+    embed.set_image(url="attachment://image.png")
+    embed.set_footer(text="有任何問題或建議請找 YellowToFish#5671")
+    await msg.channel.send(file=file, embed = embed)
+    os.remove(save_name)
 
 bot.run(os.environ['TOKEN'])
