5b9ec28a8daf302a3bd939bdefdb6dcd142de4c8
HuangYuTing <s10855033@gm2.nutn.edu.tw>
2021-07-18 04:48:52 +0800

    Update About.py

--- cogs/About.py
+++ cogs/About.py
@@ -8,7 +8,10 @@ class About(commands.Cog):
 
     @commands.command()
     async def 開發者資訊(self, ctx):
-        print()
+        embed = discord.Embed(title = "開發者資訊", color = 0xffff00)
+        embed.add_field(name = "> Name and Tag", value = "```YellowToFish#5671```")
+        embed.add_field(name = "> Support URL", value = "```https://discord.gg/fK8QhqZVMx```")
+        await ctx.channel.send(embed = embed)
 
     # @commands.command()
     # async def 開發者資訊(self, ctx):
