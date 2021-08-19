4ac3652b22aa40712e32b2a8abcad423260bece6
HuangYuTing <s10855033@gm2.nutn.edu.tw>
2021-08-19 21:24:06 +0800

    Update Search.py

--- cogs/Search.py
+++ cogs/Search.py
@@ -30,12 +30,12 @@ class Search(commands.Cog):
             if target in keywords:
                 embed.add_field(name="> 懸賞封印", value=jdata_2[target], inline=False)
                 if ctx.channel.id == 736965029642895427:
-                    ctx.channel.send(keywords)
-                    ctx.channel.send("ues")
+                    await ctx.channel.send(keywords)
+                    await ctx.channel.send("test")
             else:
                 if ctx.channel.id == 736965029642895427:
-                    ctx.channel.send(keywords)
-                    ctx.channel.send("ues")
+                    await ctx.channel.send(keywords)
+                    await ctx.channel.send("ues")
         except:
             reward_result = "```該式神無資料或輸入錯誤```"
             embed.add_field(name = "> 錯誤", value = reward_result, inline = True)
